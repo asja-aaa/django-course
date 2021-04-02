d2cfdfbfd7d17b5c774a98a2ecc10a57f7acd3ec
asja-aaa <1980490930@qq.com>
2021-04-02 14:20:53 +0800

    first functional test

---
+++ suplerlists/functional_test.py
@@ -0,0 +1,36 @@
+# coding=gbk
+from selenium import webdriver      #(1)
+import unittest
+# browser = webdriver.Chrome()      #(2)
+# # Edith has heard about a cool new online to-do app.
+# # She goesto check out its homepage
+# browser.get('http://localhost:8000')     # (3)
+# # She notices the page title and header mention to-do lists
+# assert 'To-Do' in browser.title, "Browser title was" + browser.title    #(4)
+# # She is invited to enter a to-do item straight away
+# # She types "Buy peacock feathers" into a text box (Edith's hobbyis tying fly-fishing lures
+# # when she hits enter£¬the page updates£¬and now the page lists#¡°1:Buy peacock feathers" as an item in a to-do list
+# # There is still a text box inviting her to add another item. She
+# # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+# # The page updates again£¬and now shows both items on her list
+# # Edith wonders whether the site will remember her list. Then she sees
+# # that the site has generated a unique URL for her -- there is some
+# # #explanatory text to that effect.
+# # She visits that URL - her to-do list is still there.# Satisfied£¬she goes back to sleep
+# browser.quit()
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
