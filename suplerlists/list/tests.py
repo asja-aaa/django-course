2facc5f1f33e8063a72c0f4f147658cf5215e73e
asja-aaa <1980490930@qq.com>
2021-04-09 14:00:16 +0800

    add TDD final

--- suplerlists/list/tests.py
+++ suplerlists/list/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
-
+from django.urls import resolve
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+from list.views import home_page
+from  django.http import  HttpRequest
+
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do list</title>',html)
+        self.assertTrue(html.endswith('</html>'))
+
 
